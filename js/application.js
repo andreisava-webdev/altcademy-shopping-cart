9c67d235d40b2226ad71392589275174010fd409
andreisava-webdev <andreisava.webdev@gmail.com>
2023-03-10 19:30:17 +0200

    Added total price

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
 var displayNoItems = function () {
   if ($('table tbody').children().length === 0) {
     $('<p>There are no items in the cart</p>').insertAfter('table');
+    $('#totalPrice').html('0.00');
   }
 };
 
@@ -11,12 +12,24 @@ var updateItemPrice = function (ele) {
   $(ele)
     .children('.price')
     .html('$' + price * quantity);
+
+  return price * quantity;
+};
+
+var sum = function (acc, x) {
+  return acc + x;
 };
 
 var updatePrices = function () {
+  var prices = [];
+
   $('tbody tr').each(function (i, ele) {
     var price = updateItemPrice(ele);
+    prices.push(price);
   });
+
+  var totalPrice = prices.reduce(sum).toFixed(2);
+  $('#totalPrice').html(totalPrice);
 };
 
 $(document).ready(function () {
@@ -30,7 +43,7 @@ $(document).ready(function () {
   });
 
   var timeout;
-  $(document).on('input', '[name=itemQuantity]', function () {
+  $(document).on('input', '.itemQuantity input', function () {
     clearTimeout(timeout);
     timeout = setTimeout(function () {
       updatePrices();
@@ -40,7 +53,9 @@ $(document).ready(function () {
   $('#addItem').on('submit', function (event) {
     event.preventDefault();
     var itemName = $(this).children('[name=itemName]').val();
-    var itemPrice = $(this).children('[name=itemPrice]').val();
+    var itemPrice = parseFloat(
+      $(this).children('[name=itemPrice]').val()
+    ).toFixed(2);
 
     $('tbody').append(
       '<tr>' +
@@ -51,7 +66,7 @@ $(document).ready(function () {
         itemPrice +
         '</td>' +
         '<td class="itemQuantity d-flex align-items-center">' +
-        '<input value="0" class="form-control form-control-sm mr-2" /><button class="btn btn-danger btn-sm remove">Remove</button>' +
+        '<input type="number" value="0" class="form-control form-control-sm mr-2" /><button class="btn btn-danger btn-sm remove">Remove</button>' +
         '</td>' +
         '<td class="price"></td>' +
         '</tr>'
@@ -59,5 +74,8 @@ $(document).ready(function () {
 
     displayNoItems();
     updatePrices();
+
+    $(this).children('[name=itemName]').val('');
+    $(this).children('[name=itemPrice]').val('');
   });
 });
